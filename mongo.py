6890b9852e73e758d59a41147d26e79810215a81
Irrom <nfkamil@hotmail.com>
2021-11-13 14:31:28 -0500

    mongo bugfixes and final touches

--- mongo.py
+++ mongo.py
@@ -12,7 +12,7 @@ def poll(guild):
     # 'status' is supported by all Minecraft servers that are version 1.7 or higher.
     status = server.status()
     print("The server has {0} players and replied in {1} ms".format(status.players.online, status.latency))
-    data.insert_one({'server':guild.id,'time': int(time.time()),'players':status.players.online})
+    data.insert_one({'server':guild["id"],'time': int(time.time()),'players':status.players.online})
 
 def getRange(guild_id, upper, lower):
     results = data.find({'server':guild_id,time: {'$gt':lower,'$lt':upper}})
@@ -28,7 +28,9 @@ def getServer(guild_id):
     return servers.find_one({'id': guild_id})
 
 def addServer(guild):
-    if data.find_one({'id':guild.id}) == None:
-        servers.add(guild)
+    #d1 = list(servers.find({}))
+    #d = servers.find_one({"id": guild["id"]})
+    if servers.find_one({"id": guild["id"]}) == None:
+        servers.insert_one(guild)
     else:
-        servers.find_one_and_replace({'id':guild.id}, guild)
+        servers.find_one_and_replace({'id':guild["id"]}, guild)
