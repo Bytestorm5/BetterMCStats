624939f31e6a6e71b6ab346b21861765c36c5d58
Irrom <nfkamil@hotmail.com>
2021-11-13 15:44:44 -0500

    raw data access

--- mongo.py
+++ mongo.py
@@ -8,11 +8,12 @@ data = db.get_collection("data")
 servers = db.get_collection("servers")
 
 def poll(guild):
-    server = MinecraftServer.lookup(guild["ip"] + ':' + guild["port"])
+    server = MinecraftServer.lookup(guild["ip"]) #+ ':' + guild["port"])
     # 'status' is supported by all Minecraft servers that are version 1.7 or higher.
     status = server.status()
     print("The server has {0} players and replied in {1} ms".format(status.players.online, status.latency))
-    data.insert_one({'server':guild["id"],'time': int(time.time()),'players':status.players.online})
+    data.insert_one({'server':guild["id"],'time': int(time.time()),'players':status.players.sample})
+    return status.raw
 
 def getRange(guild_id, upper, lower):
     results = data.find({'server':guild_id,time: {'$gt':lower,'$lt':upper}})
