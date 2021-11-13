8b159268945cafdc9b3db8920f58cc08894b77c4
R2bEEaton <scgryan@gmail.com>
2021-11-13 14:40:46 -0500

    main.py loop

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
+# Other things
 import discord
+import time
+import asyncio
+
+# Our things
 from mongo import *
+from graph import *
+
 TOKEN = "OTA5MTM2OTAzMzQzODM3MjA0.YY_5uA.08zxzKNAu-mN98XesrU8pP_KwJk"
+
+
+async def periodic():
+    while True:
+        for guild in client.guilds:
+            print(getServer(guild.id))
+        await asyncio.sleep(1)
+
+
+def stop():
+    global task
+    task.cancel()
+
+
+loop = asyncio.get_event_loop()
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
+        global task
+
         print('Logged on as {0}!'.format(self.user))
         activity = discord.Game(name="Minecraft", type=3)
         await client.change_presence(status=discord.Status.online, activity=activity)
+        task = loop.create_task(periodic())
 
     async def on_message(self, message):
         msg = message.content
@@ -15,15 +41,21 @@ class MyClient(discord.Client):
             if len(params) != 4:
                 await message.channel.send("!config <server_ip> <server_port> <output_channel_id>")
             else:
-                addServer({"id": message.guild.id, "ip": params[1], "port": params[2], "channel_id": params[3], "message_id": ""})
+                addServer({"id": message.guild.id, "ip": params[1], "port": params[2], "channel_id": params[3],
+                           "message_id": ""})
                 await message.channel.send("Added server successfully.")
 
-
-        if msg.startsith("!getServer"):
+        if msg.startswith("!getServer"):
             for guild in client.guilds:
                 await message.channel.send(getServer(guild.id))
 
         # await client.http.delete_message(channel_id, server_id)
+
+
+def update_stats():
+    for guild in client.guilds:
+        print(getServer(guild.id))
+
 
 client = MyClient()
 client.run(TOKEN)
